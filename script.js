e4fc5de0a375d7d8bf05e8e7d5b4626ccd820e38
Your Name <you@example.com>
2020-09-09 08:00:17 +0530

    added sound button

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 var audio = document.createElement('audio');
 audio.setAttribute('src', 'sound.mp3'); 
 audio.loop=true;
-// audio.play();
+document.body.appendChild(audio);
+
+var play = document.getElementById('soundbtn');;
+function playsound()
+{
+    console.log("inside playsound")
+    play.attributes[1].value = "javascript:stopsound()"
+    play.innerHTML = "Sound Off.."
+    audio.play();
+}
+
+function stopsound()
+{
+    console.log("inside stopsound")
+    play.attributes[1].value = "javascript:playsound()"
+    play.innerHTML = "Sound On.."
+    audio.pause();
+}
