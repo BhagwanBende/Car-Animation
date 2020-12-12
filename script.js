563f4497386e172ab9b776de5dec74334ec29f42
BhagwanBende <59956086+BhagwanBende@users.noreply.github.com>
2020-12-12 23:07:46 +0530

    typo fix

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ function playsound()
     
     play.attributes[1].value = "javascript:pausesound()"
     play.innerHTML = "Sound Off.."
+	console.log("play sound");
     audio.play();
 }
 
-function pausesound)
+function pausesound()
 {
     
     play.attributes[1].value = "javascript:playsound()"
     play.innerHTML = "Sound On.."
+	console.log("inside pause");
     audio.pause();
 }
